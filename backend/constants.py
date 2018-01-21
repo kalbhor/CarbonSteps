421b7b1036ed4e2a35714b4dc0f4b0209479244c
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2018-01-21 14:49:38 +0530

    Added emission details

---
+++ backend/constants.py
@@ -0,0 +1,927 @@
+EMISSION = {
+	"nissan": {
+		"cars": [{
+			"almera": "127 – 196 g/km"
+		}, {
+			"altima": "216 g/km"
+		}, {
+			"armada-10580891": "352 – 394 g/km"
+		}, {
+			"gt-r-86471": "275 – 298 g/km"
+		}, {
+			"juke-86513": "104 – 169 g/km"
+		}, {
+			"micra": "85 – 166 g/km"
+		}, {
+			"murano": "210 – 295 g/km"
+		}, {
+			"navara": "166 – 230 g/km"
+		}, {
+			"note-86362": "90 – 166 g/km"
+		}, {
+			"1st-generation-86690": "128 – 169 g/km"
+		}, {
+			"1st-generation-8055906": "184 – 214 g/km"
+		}, {
+			"pathfinder": "202 – 331 g/km"
+		}, {
+			"patrol": "270 – 326 g/km"
+		}, {
+			"pulsar": "94 – 134 g/km"
+		}, {
+			"qashqai": "99 – 209 g/km"
+		}, {
+			"sentra": "138 g/km"
+		}, {
+			"serena": "246 g/km"
+		}, {
+			"terrano-ii": "232 – 263 g/km"
+		}, {
+			"tiida": "52 g/km"
+		}, {
+			"x-trail-86334": "129 – 237 g/km"
+		}, {
+			"z-car": "245 – 295 g/km"
+		}, {
+			"bluebird": "191 – 222 g/km"
+		}, {
+			"cherry": "131 – 170 g/km"
+		}, {
+			"cube-10638508": "151 – 159 g/km"
+		}, {
+			"pixo-d31s-196710": "103 – 122 g/km"
+		}, {
+			"prairie-496634": "193 – 249 g/km"
+		}, {
+			"primastar": "200 g/km"
+		}, {
+			"primera": "151 – 220 g/km"
+		}, {
+			"silvia": "205 – 220 g/km"
+		}]
+	},
+	"porsche": {
+		"cars": [{
+			"911": "169 – 339 g/km"
+		}, {
+			"918-spyder-1st-generation-14871": "72 g/km"
+		}, {
+			"boxster": "158 – 281 g/km"
+		}, {
+			"cayenne-67610": "79 – 361 g/km"
+		}, {
+			"macan-95b-14900": "159 – 219 g/km"
+		}, {
+			"panamera": "56 – 242 g/km"
+		}]
+	},
+	"renault": {
+		"cars": [{
+			"captur-53695": "95 – 127 g/km"
+		}, {
+			"clio": "82 – 285 g/km"
+		}, {
+			"duster": "135 g/km"
+		}, {
+			"espace": "119 – 292 g/km"
+		}, {
+			"fluence-238277": "0 – 182 g/km"
+		}, {
+			"kadjar-1st-generation-6370029": "99 – 129 g/km"
+		}, {
+			"kangoo": "112 – 195 g/km"
+		}, {
+			"koleos-238173": "120 – 237 g/km"
+		}, {
+			"latitude-1st-generation-35011": "152 – 250 g/km"
+		}, {
+			"master": "190 – 222 g/km"
+		}, {
+			"megane": "86 – 214 g/km"
+		}, {
+			"scenic": "100 – 224 g/km"
+		}, {
+			"1st-generation-7123055": "95 – 130 g/km"
+		}, {
+			"trafic": "149 – 200 g/km"
+		}, {
+			"twingo": "90 – 167 g/km"
+		}, {
+			"14-1st-generation-552038": "172 – 181 g/km"
+		}, {
+			"1st-generation-551825": "144 – 216 g/km"
+		}, {
+			"19": "177 g/km"
+		}, {
+			"25-1st-generation-443899": "163 – 233 g/km"
+		}, {
+			"avantime-1st-generation-239220": "193 – 271 g/km"
+		}, {
+			"laguna": "109 – 271 g/km"
+		}, {
+			"modus": "119 – 180 g/km"
+		}, {
+			"safrane-239991": "195 – 252 g/km"
+		}, {
+			"vel-satis-239834": "188 – 275 g/km"
+		}, {
+			"wind-1st-generation-239177": "145 – 159 g/km"
+		}]
+	},
+	"skoda": {
+		"cars": [{
+			"1st-generation-49061": "79 – 106 g/km"
+		}, {
+			"fabia": "12 – 187 g/km"
+		}, {
+			"karoq-1st-generation-11259249": "115 – 138 g/km"
+		}, {
+			"kodiaq-1st-generation-10503386": "131 – 168 g/km"
+		}, {
+			"octavia": "87 – 242 g/km"
+		}, {
+			"rapid": "89 – 137 g/km"
+		}, {
+			"superb": "100 – 253 g/km"
+		}, {
+			"yeti": "118 – 189 g/km"
+		}, {
+			"1st-generation-10089441": "16 – 163 g/km"
+		}, {
+			"1st-generation-192491": "47 – 160 g/km"
+		}, {
+			"felicia": "154 – 169 g/km"
+		}, {
+			"roomster": "109 – 190 g/km"
+		}]
+	},
+	"suzuki": {
+		"cars": [{
+			"alto": "99 – 172 g/km"
+		}, {
+			"baleno": "158 – 220 g/km"
+		}, {
+			"celerio-1st-generation-76932": "84 – 99 g/km"
+		}, {
+			"ignis": "107 – 174 g/km"
+		}, {
+			"jimny": "162 – 167 g/km"
+		}, {
+			"swift": "97 – 177 g/km"
+		}, {
+			"sx4": "110 – 182 g/km"
+		}, {
+			"vitara": "106 – 131 g/km"
+		}, {
+			"wagon-r": "143 – 145 g/km"
+		}, {
+			"cappuccino-1st-generation-299279": "158 g/km"
+		}, {
+			"grand-vitara": "174 – 285 g/km"
+		}, {
+			"kizashi-1st-generation-72640": "183 – 191 g/km"
+		}, {
+			"liana": "141 – 192 g/km"
+		}, {
+			"splash-1st-generation-72577": "109 – 142 g/km"
+		}]
+	},
+	"volkswagen": {
+		"cars": [{
+			"amarok": "187 – 228 g/km"
+		}, {
+			"arteon-1st-generation-10982231": "114 – 164 g/km"
+		}, {
+			"beetle": "110 – 286 g/km"
+		}, {
+			"caddy": "98 – 199 g/km"
+		}, {
+			"golf": "0 – 282 g/km"
+		}, {
+			"jetta": "95 – 199 g/km"
+		}, {
+			"passat": "37 – 317 g/km"
+		}, {
+			"passat-nms-4-door-10827424": "134 – 215 g/km"
+		}, {
+			"polo": "87 – 221 g/km"
+		}, {
+			"scirocco-51993": "109 – 205 g/km"
+		}, {
+			"sharan-51900": "130 – 326 g/km"
+		}, {
+			"t-roc-1st-generation-16397671": "116 – 152 g/km"
+		}, {
+			"taigun-1st-generation-7123254": "110 g/km"
+		}, {
+			"tiguan": "119 – 234 g/km"
+		}, {
+			"touareg-51632": "176 – 375 g/km"
+		}, {
+			"touran": "109 – 228 g/km"
+		}, {
+			"transporter": "155 – 322 g/km"
+		}, {
+			"up": "0 – 106 g/km"
+		}, {
+			"vento-217231": "137 – 282 g/km"
+		}, {
+			"cc-53215": "113 – 213 g/km"
+		}, {
+			"eos-53049": "125 – 219 g/km"
+		}, {
+			"phaeton": "224 – 374 g/km"
+		}, {
+			"a4-typ-1j-217481": "135 – 264 g/km"
+		}, {
+			"derby-218871": "191 – 193 g/km"
+		}, {
+			"5z-215262": "130 – 159 g/km"
+		}, {
+			"lupo-1st-generation-215112": "81 – 175 g/km"
+		}]
+	},
+	"volvo": {
+		"cars": [{
+			"s60": "99 – 266 g/km"
+		}, {
+			"1st-generation-6518073": "46 – 169 g/km"
+		}, {
+			"v40": "85 – 233 g/km"
+		}, {
+			"v60": "47 – 237 g/km"
+		}, {
+			"1st-generation-6518076": "46 – 177 g/km"
+		}, {
+			"xc40-1st-generation-16315111": "133 – 166 g/km"
+		}, {
+			"xc60-182948": "49 – 284 g/km"
+		}, {
+			"xc90-181412": "49 – 381 g/km"
+		}, {
+			"s80-183601": "104 – 284 g/km"
+		}, {
+			"v70-181952": "70 – 280 g/km"
+		}, {
+			"xc70-181854": "70 – 279 g/km"
+		}, {
+			"850": "212 – 220 g/km"
+		}, {
+			"900": "237 g/km"
+		}, {
+			"c30-183098": "0 – 224 g/km"
+		}, {
+			"c70": "154 – 268 g/km"
+		}, {
+			"s40-180486": "99 – 241 g/km"
+		}, {
+			"s70-1st-generation-183824": "168 – 270 g/km"
+		}, {
+			"v50-181589": "99 – 243 g/km"
+		}]
+	},
+	"audi": {
+		"cars": [{
+			"a1": "89 – 166 g/km"
+		}, {
+			"a3": "35 – 254 g/km"
+		}, {
+			"a4": "99 – 334 g/km"
+		}, {
+			"a5": "107 – 288 g/km"
+		}, {
+			"a6": "52 – 350 g/km"
+		}, {
+			"a7-sportback-84695": "122 – 225 g/km"
+		}, {
+			"a8": "144 – 353 g/km"
+		}, {
+			"q2-1st-generation-7123235": "109 – 139 g/km"
+		}, {
+			"q3-84963": "116 – 206 g/km"
+		}, {
+			"q5-84965": "117 – 218 g/km"
+		}, {
+			"q7-84967": "48 – 317 g/km"
+		}, {
+			"r8": "227 – 351 g/km"
+		}, {
+			"tt": "110 – 257 g/km"
+		}, {
+			"concept-554982": "140 g/km"
+		}, {
+			"80": "123 – 249 g/km"
+		}, {
+			"a2-8z-95940": "81 – 142 g/km"
+		}, {
+			"cabriolet-b4-113688": "162 – 268 g/km"
+		}, {
+			"coupe-113328": "205 – 219 g/km"
+		}]
+	},
+	"aston-martin": {
+		"cars": [{
+			"1st-generation-8111009": "230 – 265 g/km"
+		}, {
+			"db9": "333 – 421 g/km"
+		}, {
+			"rapide-71155": "300 – 355 g/km"
+		}, {
+			"vanquish": "298 – 343 g/km"
+		}, {
+			"vantage": "296 – 395 g/km"
+		}, {
+			"cygnet-1st-generation-1219": "113 – 120 g/km"
+		}, {
+			"1st-generation-174106": "351 – 469 g/km"
+		}, {
+			"dbs": "367 – 388 g/km"
+		}, {
+			"one-77-1st-generation-71012": "572 g/km"
+		}, {
+			"v12-zagato-1st-generation-71144": "388 g/km"
+		}, {
+			"virage": "349 g/km"
+		}]
+	},
+	"bmw": {
+		"cars": [{
+			"1-series": "96 – 226 g/km"
+		}, {
+			"2-series": "46 – 199 g/km"
+		}, {
+			"3-series": "44 – 309 g/km"
+		}, {
+			"4-series": "106 – 213 g/km"
+		}, {
+			"5-series": "44 – 361 g/km"
+		}, {
+			"6-series": "129 – 366 g/km"
+		}, {
+			"7-series": "49 – 332 g/km"
+		}, {
+			"i3-1975": "0 – 14 g/km"
+		}, {
+			"i8": "42 – 46 g/km"
+		}, {
+			"x1": "104 – 223 g/km"
+		}, {
+			"x2-1st-generation-7123245": "121 – 133 g/km"
+		}, {
+			"x3-58519": "123 – 293 g/km"
+		}, {
+			"x4-f26-359000": "136 – 199 g/km"
+		}, {
+			"x5-58714": "77 – 356 g/km"
+		}, {
+			"x6-58592": "157 – 325 g/km"
+		}, {
+			"z4": "159 – 292 g/km"
+		}, {
+			"8-series-e31-150511": "304 – 333 g/km"
+		}, {
+			"e3-151792": "440 g/km"
+		}, {
+			"z3": "187 – 268 g/km"
+		}, {
+			"z8-e52-150884": "349 g/km"
+		}]
+	},
+	"cadillac": {
+		"cars": [{
+			"ats": "174 – 265 g/km"
+		}, {
+			"cts": "180 – 370 g/km"
+		}, {
+			"escalade": "264 – 451 g/km"
+		}, {
+			"srx-60941": "230 – 331 g/km"
+		}, {
+			"xt5-1st-generation-8060099": "229 g/km"
+		}, {
+			"1st-generation-229810": "156 – 279 g/km"
+		}, {
+			"seville-351941": "315 – 335 g/km"
+		}, {
+			"sts-229724": "295 – 379 g/km"
+		}, {
+			"xlr-233182": "291 – 339 g/km"
+		}]
+	},
+	"chevrolet": {
+		"cars": [{
+			"aveo": "97 – 184 g/km"
+		}, {
+			"camaro": "181 – 329 g/km"
+		}, {
+			"captiva-71834": "164 – 264 g/km"
+		}, {
+			"corvette": "282 – 355 g/km"
+		}, {
+			"cruze": "117 – 184 g/km"
+		}, {
+			"malibu": "157 – 183 g/km"
+		}, {
+			"orlando-1st-generation-71474": "139 – 186 g/km"
+		}, {
+			"spark-71371": "0 – 119 g/km"
+		}, {
+			"tahoe": "366 g/km"
+		}, {
+			"trax-71438": "119 – 158 g/km"
+		}, {
+			"volt-71416": "27 g/km"
+		}, {
+			"alero-1st-generation-6375181": "224 – 286 g/km"
+		}, {
+			"beretta-1st-generation-368758": "172 – 202 g/km"
+		}, {
+			"corsica": "202 – 256 g/km"
+		}, {
+			"epica-v250-385740": "169 – 221 g/km"
+		}, {
+			"evanda-1st-generation-6924609": "213 – 241 g/km"
+		}, {
+			"hhr-1st-generation-384122": "207 – 214 g/km"
+		}, {
+			"1st-generation-6413010": "149 – 218 g/km"
+		}, {
+			"1st-generation-385464": "146 – 210 g/km"
+		}, {
+			"s-10-blazer": "316 – 335 g/km"
+		}]
+	},
+	"citroen": {
+		"cars": [{
+			"berlingo": "0 – 195 g/km"
+		}, {
+			"c1": "88 – 109 g/km"
+		}, {
+			"c3": "79 – 172 g/km"
+		}, {
+			"c3-aircross-1st-generation-11453159": "104 – 126 g/km"
+		}, {
+			"c3-picasso-44434": "101 – 149 g/km"
+		}, {
+			"c4": "86 – 200 g/km"
+		}, {
+			"c4-aircross-1st-generation-166579": "119 – 192 g/km"
+		}, {
+			"c4-cactus-76421": "82 – 107 g/km"
+		}, {
+			"c4-picasso": "98 – 211 g/km"
+		}, {
+			"c5": "106 – 250 g/km"
+		}, {
+			"elysee-431689": "112 – 168 g/km"
+		}, {
+			"jumper": "153 – 226 g/km"
+		}, {
+			"jumpy": "133 – 216 g/km"
+		}, {
+			"1st-generation-45004": "85 – 153 g/km"
+		}, {
+			"c-crosser-1st-generation-166526": "185 – 224 g/km"
+		}, {
+			"c2-166131": "108 – 163 g/km"
+		}, {
+			"c6-1st-generation-166349": "175 – 266 g/km"
+		}, {
+			"c8-1st-generation-166403": "155 – 275 g/km"
+		}, {
+			"cx": "156 – 249 g/km"
+		}, {
+			"ds4-1st-generation-45140": "110 – 178 g/km"
+		}, {
+			"ds5-1st-generation-45227": "90 – 165 g/km"
+		}, {
+			"evasion-1st-generation-166670": "186 – 262 g/km"
+		}, {
+			"gs": "181 g/km"
+		}, {
+			"1st-generation-431797": "109 – 155 g/km"
+		}, {
+			"saxo": "138 – 159 g/km"
+		}, {
+			"xsara-picasso-1st-generation-168001": "136 – 172 g/km"
+		}]
+	},
+	"daewoo": {
+		"cars": [{
+			"lacetti": "169 – 218 g/km"
+		}, {
+			"evanda-1st-generation-6364864": "213 – 241 g/km"
+		}, {
+			"t200-278497": "159 – 178 g/km"
+		}, {
+			"lanos": "189 – 238 g/km"
+		}, {
+			"leganza-1st-generation-8051478": "65 – 251 g/km"
+		}, {
+			"matiz-277734": "153 – 160 g/km"
+		}, {
+			"nubira": "166 – 251 g/km"
+		}, {
+			"tacuma-1st-generation-277788": "208 – 265 g/km"
+		}, {
+			"tosca-1st-generation-11434364": "186 g/km"
+		}, {
+			"winstorm-1st-generation-11117105": "216 – 271 g/km"
+		}]
+	},
+	"datsun": {
+		"cars": [{
+			"bluebird": "207 g/km"
+		}, {
+			"cherry": "186 g/km"
+		}, {
+			"z-car": "235 – 256 g/km"
+		}]
+	},
+	"dc-design": {
+		"cars": []
+	},
+	"fiat": {
+		"cars": [{
+			"124": "148 – 153 g/km"
+		}, {
+			"500": "88 – 140 g/km"
+		}, {
+			"1st-generation-89405": "104 – 145 g/km"
+		}, {
+			"500x-1st-generation-367491": "107 – 157 g/km"
+		}, {
+			"doblo": "124 – 173 g/km"
+		}, {
+			"ducato": "179 – 239 g/km"
+		}, {
+			"fiorino": "103 – 161 g/km"
+		}, {
+			"freemont-jc49-87320": "169 – 262 g/km"
+		}, {
+			"1st-generation-7841712": "169 – 189 g/km"
+		}, {
+			"linea-323-258738": "123 – 148 g/km"
+		}, {
+			"palio": "171 – 198 g/km"
+		}, {
+			"panda": "86 – 155 g/km"
+		}, {
+			"punto": "85 – 201 g/km"
+		}, {
+			"tipo": "98 – 147 g/km"
+		}, {
+			"uno": "146 g/km"
+		}, {
+			"131": "210 g/km"
+		}, {
+			"barchetta-260114": "198 – 201 g/km"
+		}, {
+			"bravo": "120 – 224 g/km"
+		}, {
+			"coupe-175-259304": "205 – 241 g/km"
+		}, {
+			"croma-260463": "160 – 229 g/km"
+		}, {
+			"idea-258834": "135 – 157 g/km"
+		}, {
+			"185-261342": "149 – 253 g/km"
+		}, {
+			"multipla-258929": "170 – 205 g/km"
+		}, {
+			"138-264390": "148 – 188 g/km"
+		}, {
+			"ritmo": "207 – 214 g/km"
+		}, {
+			"sedici-6338577": "129 – 174 g/km"
+		}, {
+			"seicento-262348": "146 – 148 g/km"
+		}, {
+			"stilo": "139 – 233 g/km"
+		}, {
+			"ulysse-259067": "182 – 275 g/km"
+		}]
+	},
+	"ford": {
+		"cars": [{
+			"b-max-mk1-38110": "98 – 149 g/km"
+		}, {
+			"c-max": "99 – 194 g/km"
+		}, {
+			"ecosport-68660": "120 – 149 g/km"
+		}, {
+			"edge-90302": "149 – 152 g/km"
+		}, {
+			"everest-10967921": "182 – 224 g/km"
+		}, {
+			"explorer": "137 – 327 g/km"
+		}, {
+			"fiesta": "89 – 179 g/km"
+		}, {
+			"focus": "0 – 237 g/km"
+		}, {
+			"galaxy-41597": "129 – 331 g/km"
+		}, {
+			"ka": "110 – 189 g/km"
+		}, {
+			"kuga-40241": "115 – 244 g/km"
+		}, {
+			"mondeo": "94 – 257 g/km"
+		}, {
+			"mustang": "179 – 287 g/km"
+		}, {
+			"ranger": "171 – 281 g/km"
+		}, {
+			"s-max-38614": "129 – 232 g/km"
+		}, {
+			"territory-10552582": "75 – 232 g/km"
+		}, {
+			"transit": "124 – 244 g/km"
+		}, {
+			"transit-connect": "115 – 184 g/km"
+		}, {
+			"1st-generation-10711750": "104 – 120 g/km"
+		}, {
+			"transit-custom": "178 – 182 g/km"
+		}, {
+			"capri-290546": "258 g/km"
+		}, {
+			"cougar-290467": "196 – 229 g/km"
+		}, {
+			"escort": "152 – 203 g/km"
+		}, {
+			"fusion-290024": "116 – 184 g/km"
+		}, {
+			"granada": "237 g/km"
+		}, {
+			"maverick": "230 – 286 g/km"
+		}, {
+			"probe-290362": "181 g/km"
+		}, {
+			"puma-1st-generation-290320": "171 – 178 g/km"
+		}, {
+			"sierra": "226 – 281 g/km"
+		}]
+	},
+	"hindustan": {
+		"cars": [{
+			"ambassador-10166013": "202 g/km"
+		}]
+	},
+	"honda": {
+		"cars": [{
+			"accord": "138 – 237 g/km"
+		}, {
+			"city": "132 – 156 g/km"
+		}, {
+			"civic": "94 – 215 g/km"
+		}, {
+			"cr-v-73272": "115 – 235 g/km"
+		}, {
+			"cr-z-73329": "117 – 122 g/km"
+		}, {
+			"freed-541482": "108 – 176 g/km"
+		}, {
+			"hr-v": "104 – 205 g/km"
+		}, {
+			"jazz-73084": "64 – 144 g/km"
+		}, {
+			"legend": "269 – 282 g/km"
+		}, {
+			"n-one-jg1-2-281422": "86 – 104 g/km"
+		}, {
+			"nsx-282493": "264 – 291 g/km"
+		}, {
+			"odyssey-6340344": "178 – 233 g/km"
+		}, {
+			"s660-1st-generation-9734279": "96 – 110 g/km"
+		}, {
+			"vezel-1st-generation-224216": "89 – 122 g/km"
+		}, {
+			"fr-v-285615": "167 – 199 g/km"
+		}, {
+			"insight-73240": "80 – 101 g/km"
+		}, {
+			"1st-generation-283645": "150 – 170 g/km"
+		}, {
+			"prelude-281954": "220 – 248 g/km"
+		}, {
+			"s2000-221913": "236 – 237 g/km"
+		}, {
+			"stream-285425": "157 – 216 g/km"
+		}]
+	},
+	"hyundai": {
+		"cars": [{
+			"accent": "146 – 192 g/km"
+		}, {
+			"elantra": "53 – 211 g/km"
+		}, {
+			"eon-1st-generation-116595": "114 g/km"
+		}, {
+			"grandeur-118084": "208 – 245 g/km"
+		}, {
+			"i10": "98 – 142 g/km"
+		}, {
+			"i20": "84 – 155 g/km"
+		}, {
+			"i30": "94 – 182 g/km"
+		}, {
+			"i40": "110 – 176 g/km"
+		}, {
+			"ioniq-1st-generation-8106009": "0 – 26 g/km"
+		}, {
+			"ix20-114528": "110 – 154 g/km"
+		}, {
+			"ix25-1st-generation-7842982": "161 – 188 g/km"
+		}, {
+			"kona-1st-generation-11428400": "117 – 153 g/km"
+		}, {
+			"santa-fe": "154 – 274 g/km"
+		}, {
+			"sonata-116622": "159 – 241 g/km"
+		}, {
+			"tucson-117093": "0 – 237 g/km"
+		}, {
+			"veloster-113937": "137 – 165 g/km"
+		}, {
+			"equus": "201 – 244 g/km"
+		}, {
+			"genesis-115356": "235 – 270 g/km"
+		}, {
+			"genesis-coupe-424415": "220 – 246 g/km"
+		}, {
+			"atos": "131 – 174 g/km"
+		}, {
+			"coupe-118575": "193 – 250 g/km"
+		}, {
+			"excel": "149 – 174 g/km"
+		}, {
+			"getz": "118 – 183 g/km"
+		}, {
+			"ix35-114410": "0 – 233 g/km"
+		}, {
+			"ix55-1st-generation-114343": "199 – 249 g/km"
+		}, {
+			"lantra": "149 – 207 g/km"
+		}, {
+			"matrix-118369": "142 – 219 g/km"
+		}, {
+			"s-coupe-118026": "193 g/km"
+		}, {
+			"terracan-1st-generation-117939": "222 – 365 g/km"
+		}, {
+			"trajet-117828": "192 – 273 g/km"
+		}]
+	},
+	"lexus": {
+		"cars": [{
+			"ct-74778": "87 – 94 g/km"
+		}, {
+			"es-74482": "224 – 267 g/km"
+		}, {
+			"gs-74511": "104 – 296 g/km"
+		}, {
+			"is": "99 – 270 g/km"
+		}, {
+			"lc-1st-generation-8107445": "250 g/km"
+		}, {
+			"ls": "141 – 270 g/km"
+		}, {
+			"lx-74843": "250 – 357 g/km"
+		}, {
+			"nx-445996": "116 – 178 g/km"
+		}, {
+			"rc-1st-generation-74758": "116 – 251 g/km"
+		}, {
+			"rx": "140 – 311 g/km"
+		}, {
+			"lfa-1st-generation-74914": "379 g/km"
+		}, {
+			"sc": "287 g/km"
+		}]
+	},
+	"land-rover": {
+		"cars": [{
+			"discovery": "159 – 397 g/km"
+		}, {
+			"discovery-sport-1st-generation-420366": "123 – 191 g/km"
+		}, {
+			"range-rover": "64 – 396 g/km"
+		}, {
+			"range-rover-evoque": "109 – 201 g/km"
+		}, {
+			"range-rover-sport-68261": "64 – 376 g/km"
+		}, {
+			"range-rover-velar-1st-generation-11004809": "142 – 214 g/km"
+		}, {
+			"defender": "228 – 295 g/km"
+		}, {
+			"freelander": "158 – 298 g/km"
+		}]
+	},
+	"mahindra": {
+		"cars": [{
+			"genio": "210 – 243 g/km"
+		}, {
+			"kuv100-1st-generation-11261342": "117 – 139 g/km"
+		}, {
+			"quanto-1st-generation-6423092": "200 g/km"
+		}, {
+			"thar-1st-generation-8122952": "237 g/km"
+		}]
+	},
+	"maruti": {
+		"cars": []
+	},
+	"mazda": {
+		"cars": [{
+			"2": "89 – 168 g/km"
+		}, {
+			"3": "99 – 231 g/km"
+		}, {
+			"5-43325": "138 – 198 g/km"
+		}, {
+			"6": "104 – 250 g/km"
+		}, {
+			"biante-1st-generation-276910": "157 – 247 g/km"
+		}, {
+			"cx-3-1st-generation-6333618": "105 – 150 g/km"
+		}, {
+			"cx-5-43743": "119 – 165 g/km"
+		}, {
+			"cx-7-43806": "199 – 243 g/km"
+		}, {
+			"cx-9-43846": "197 – 309 g/km"
+		}, {
+			"mpv": "188 – 241 g/km"
+		}, {
+			"mx-5-43469": "139 – 229 g/km"
+		}, {
+			"121": "137 – 162 g/km"
+		}, {
+			"323": "135 – 219 g/km"
+		}, {
+			"626": "152 – 217 g/km"
+		}, {
+			"929": "205 – 221 g/km"
+		}, {
+			"demio-1st-generation-285971": "167 – 190 g/km"
+		}, {
+			"premacy-276778": "177 – 225 g/km"
+		}, {
+			"rx-8-11438": "267 – 284 g/km"
+		}, {
+			"tribute-277163": "223 – 305 g/km"
+		}, {
+			"xedos-6-1st-generation-277093": "181 – 220 g/km"
+		}]
+	},
+	"merceded-benz": {
+		"cars": []
+	},
+	"mini": {
+		"cars": [{
+			"countryman": "52 – 184 g/km"
+		}, {
+			"hatch": "0 – 216 g/km"
+		}, {
+			"1st-generation-75953": "114 – 169 g/km"
+		}]
+	},
+	"mitsubishi": {
+		"cars": [{
+			"asx-63470": "119 – 170 g/km"
+		}, {
+			"eclipse-cross-1st-generation-11017147": "159 g/km"
+		}, {
+			"l200": "169 – 242 g/km"
+		}, {
+			"lancer": "128 – 328 g/km"
+		}, {
+			"mirage": "95 – 101 g/km"
+		}, {
+			"outlander-34762": "42 – 240 g/km"
+		}, {
+			"pajero": "60 – 341 g/km"
+		}, {
+			"pajero-sport-341910": "275 – 310 g/km"
+		}, {
+			"space-star-45934": "92 – 198 g/km"
+		}, {
+			"carisma": "152 – 183 g/km"
+		}, {
+			"colt": "115 – 192 g/km"
+		}, {
+			"eclipse": "225 g/km"
+		}, {
+			"galant": "195 – 225 g/km"
+		}, {
+			"grandis-1st-generation-340775": "176 – 237 g/km"
+		}, {
+			"space-wagon-341253": "198 – 258 g/km"
+		}, {
+			"starion-1st-generation-6344654": "223 – 247 g/km"
+		}]
+	}
+}
